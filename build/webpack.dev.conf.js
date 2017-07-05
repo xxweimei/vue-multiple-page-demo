a6c1715b8355d3d5234eeaca4ae8f97cd6ca1e13
shengyang <123456>
2017-07-05 15:17:02 +0800

    init

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,42 @@
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+
+var modulemap = require('../modulemap')
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
+
+var currentConfig = {
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new FriendlyErrorsPlugin()
+  ]
+}
+
+for (let key in modulemap) {
+  currentConfig.plugins.push(new HtmlWebpackPlugin({
+    filename: key + '.html',
+    template: './src/module/' + key + '/' + key + '.html',
+    inject: true,
+    chunks: modulemap[key].chunks
+  }))
+}
+
+module.exports = merge(baseWebpackConfig, currentConfig)
---
+++ src/module/editor/Editor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="editor">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+  import Hello from '../../components/Hello'
+
+  export default {
+    name: 'editor',
+    components: {
+      Hello
+    }
+  }
+</script>
